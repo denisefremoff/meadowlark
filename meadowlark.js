f0243f88e15b479935dacfc28bfe6687c8d8412b
Денис <denisefremoff@mail.ru>
2024-04-23 18:49:54 +0300

    форма

--- meadowlark.js
+++ meadowlark.js
@@ -9,20 +9,24 @@ const app = express();
 app.engine('handlebars', engine({
 	defaultLayout: 'main',
 }));
-
 app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', handlers.home);
-
 app.get('/about', handlers.about);
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup); // Проверьте правильность пути
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 
 // Пользовательская страница 404
 app.use(handlers.notFound);
-
 // Пользовательская страница 500
 app.use(handlers.serverError);
 
